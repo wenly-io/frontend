70079f671dfc91c8c38e479fa8695602a8cf2513
mfonPeeter <godimfon@gmail.com>
2026-01-17 21:46:51 +0100

    feat: implement dashboard layout with responsiveness

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,5 @@
 export { default as GoogleIcon } from './google-icon';
 export { default as SyncArrowIcon } from './sync-arrow-icon';
+export { default as SwirlIcon } from './swirl-icon';
+export { default as WhatsAppIcon } from './whatsapp-icon';
+export { default as WenlyLogoIcon } from './wenly-logo-icon';
